8ad39fced809a60871b844c40dac4b3769c5fa76
Samuel Gabel <smlgbl@gmail.com>
2013-07-06 23:55:18 +0200

    Add 'add', 'replace' and 'extend' special options Add fitting tests Ignore VIM swap files

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,14 +5,29 @@ module.exports = function(grunt) {
   grunt.initConfig({
     env : {
       options: {
-        globalOption : 'foo'
+        add: {
+          globalOption : 'foo'
+        },
+        replace: {
+          USER: 'horst'
+        },
+        extend: {
+          PATH: {
+            value: 'node_modules/.bin',
+            delimiter: ':'
+          },
+          HOME: {
+            value: 'WHATEVER'
+          }
+        }
       },
       testData : {
         data : 'bar'
       },
       testOptions : {
         options : {
-          localOption : 'baz'
+          localOption : 'baz',
+          USER: 'fritz'
         }
       },
       testDotEnv : {
@@ -40,6 +55,13 @@ module.exports = function(grunt) {
   grunt.registerTask('testData', function(){
     assert.equal(process.env.globalOption, 'foo', 'globalOption should be set');
     assert.equal(process.env.data, 'bar', 'data should be set');
+    assert.equal(process.env.USER, 'horst', 'USER should be set to "horst"');
+    assert(process.env.PATH.indexOf('node_modules/.bin') >= 0, 'PATH should contain "node_modules/.bin"');
+    assert(process.env.PATH.indexOf('node_modules/.bin:') >= 0, 'PATH should contain "node_modules/.bin:"');
+    assert(process.env.HOME.indexOf('WHATEVER') >= 0, 'HOME should contain "WHATEVER"');
+    assert(process.env.HOME.indexOf('WHATEVER') >= 7, 'HOME should start with "/home/" and end in "WHATEVER"');
+    assert(process.env.HOME.indexOf('/home/') === 0, 'HOME should start with "/home/" and end in "WHATEVER"');
+    assert(process.env.HOME.indexOf('WHATEVER') === process.env.HOME.length - 8, 'HOME should start with "/home/" and end in "WHATEVER"');
     delete process.env.globalOption;
     delete process.env.data;
   });
@@ -47,19 +69,30 @@ module.exports = function(grunt) {
   grunt.registerTask('testOptions', function(){
     assert.equal(process.env.globalOption, 'foo', 'globalOption should be set');
     assert.equal(process.env.localOption, 'baz', 'localOption should be set');
+    assert.equal(process.env.USER, 'fritz', 'USER should be set to "fritz"');
+    assert(process.env.PATH.indexOf('node_modules/.bin') >= 0, 'PATH should contain "node_modules/.bin"');
+    assert(process.env.PATH.indexOf('node_modules/.bin:') >= 0, 'PATH should contain "node_modules/.bin:"');
+    assert(process.env.HOME.indexOf('WHATEVER') >= 0, 'HOME should contain "WHATEVER"');
+    assert(process.env.HOME.indexOf('WHATEVER') >= 7, 'HOME should start with "/home/" and end in "WHATEVER"');
+    assert(process.env.HOME.indexOf('/home/') === 0, 'HOME should start with "/home/" and end in "WHATEVER"');
+    assert(process.env.HOME.lastIndexOf('WHATEVER') === process.env.HOME.length - 8, 'HOME should start with "/home/" and end in "WHATEVER"');
+    assert(process.env.HOME.indexOf('WHATEVER') === process.env.HOME.length - 16, 'HOME should start with "/home/" and end in "WHATEVER"');
+    assert(process.env.HOME.indexOf('WHATEVERWHATEVER') === process.env.HOME.length - 16, 'HOME should start with "/home/" and end in "WHATEVERWHATEVER"');
     delete process.env.globalOption;
     delete process.env.localOption;
   });
 
   grunt.registerTask('writeDotEnv', function(){
-    grunt.file.write('.env', "dotEnvFileData=bar\ndotEnvFileOption=baz\n");
-    grunt.file.write('.env.json', '{"jsonValue" : "foo"}');
+    grunt.file.write('.env', "dotEnvFileData=bar\ndotEnvFileOption=baz\nUSER=horst");
+    grunt.file.write('.env.json', '{"jsonValue" : "foo","extend" : {"PATH":"jsonPath"}}');
   });
 
   grunt.registerTask('testDotEnv', function(){
     assert(!process.env.src, 'Should not include src');
     assert.equal(process.env.jsonValue, 'foo', 'value from json env file should be set');
+    assert(process.env.PATH.indexOf('jsonPath') >= 0, 'PATH should contain "jsonPath"');
     assert.equal(process.env.dotEnvFileData, 'bar', 'dotEnvFileData should be set');
+    assert.equal(process.env.USER, 'horst', 'USER should be set to "horst"');
     assert.equal(process.env.dotEnvFileOption, 'baz', 'dotEnvFileOption should be set');
     delete process.env.jsonValue;
     delete process.env.dotEnvFileData;
--- tasks/env.js
+++ tasks/env.js
@@ -13,19 +13,65 @@ module.exports = function (grunt) {
 
   grunt.registerMultiTask('env', 'Specify an ENV configuration for future tasks in the chain', function() {
 
+    extendEnv(this.options());
     var data = grunt.util._.clone(this.data);
     delete data.src;
-
-    grunt.util._.extend(process.env, this.options(), data);
+    extendEnv(data);
 
     if (this.files.length) {
       this.files[0].src.forEach(function(file){
         var fileContent = grunt.file.read(file);
         var data = readJson(fileContent) || readIni(fileContent) || {};
-        grunt.util._.extend(process.env, data);
+        extendEnv(data);
       });
     }
   });
+
+  function extendEnv(options) {
+    grunt.util._.forEach(options, function(optionData, option) {
+      if(option === 'add' && typeof optionData === 'object') {
+        grunt.util._.forEach(optionData, function(value, key) {
+          if(process.env[key]) {
+            grunt.log.writeln('Not adding "' + key + '", because it already exists. Maybe you wanted to put it under the "replace" or "extend" options?');
+          } else {
+            var data = {};
+            data[key] = value;
+            grunt.util._.extend(process.env, data);
+          }
+        });
+      } else if(option === 'replace' && typeof optionData === 'object') {
+        grunt.util._.forEach(optionData, function(value, key) {
+          if(process.env[key]) {
+            process.env[key] = value;
+          } else {
+            grunt.log.writeln('Not replacing "' + key + '", because it doesn\'t exist. Maybe you wanted to put it under the "add" options?');
+          }
+        });
+      } else if(option === 'extend' && typeof optionData === 'object') {
+        grunt.util._.forEach(optionData, function(value, key) {
+          if(process.env[key]) {
+            if(typeof value === 'object') {
+              if(value.delimiter) {
+                var array = process.env[key].split(value.delimiter);
+                array.unshift(value.value);
+                process.env[key] = array.join(value.delimiter);
+              } else {
+                process.env[key] += value.value;
+              }
+            } else {
+              process.env[key] += value;
+            }
+          } else {
+            grunt.log.writeln('Not extending "' + key + '", because it doesn\'t exist. Maybe you wanted to put it under the "add" options?');
+          }
+        });
+      } else {
+        var data = {};
+        data[option] = optionData;
+        grunt.util._.extend(process.env, data);
+      }
+    });
+  }
 };
 
 function readJson(content) {
@@ -43,5 +89,4 @@ function readIni(content) {
     return;
   }
 }
-
 
