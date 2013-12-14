97b4b8cbdd89b27ca4d0f363291e263d3f75337e
Jarrod Overson <jsoverson@gmail.com>
2013-12-13 22:12:36 -0800

    added tests for directives

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,57 @@
 module.exports = function(grunt) {
   "use strict";
 
+  process.env.TEST = 'test';
+
   // Project configuration.
   grunt.initConfig({
     env : {
-      options: {
+      options : {
         globalOption : 'foo'
       },
       testData : {
+        TEST : 'test',
         data : 'bar'
       },
       testOptions : {
         options : {
-          localOption : 'baz'
+          localOption : 'baz',
+          USER: 'fritz'
         }
       },
       testDotEnv : {
         src : ['.env', '.env.json']
+      },
+      testDirectives : {
+        ADD_NEGATIVE : 'should not change',
+        PATHLIKE : 'foo:bar:baz',
+        UNSHIFTBASIC : 'bar',
+        UNSHIFTOBJECT : 'bar',
+        REPLACE : 'foo',
+
+        options : {
+          add : {
+            ADD_POSITIVE : 'should be this',
+            ADD_NEGATIVE : 'should not change to this'
+          },
+          replace : {
+            REPLACE : 'bar',
+            REPLACE_NEGATIVE : '',
+          },
+          push : {
+            PATHLIKE : {
+              value : 'qux',
+              delimiter : ':'
+            }
+          },
+          unshift : {
+            UNSHIFTBASIC : 'foo',
+            UNSHIFTOBJECT : {
+              value : 'foo'
+            }
+
+          }
+        }
       }
     },
     clean : {
@@ -51,19 +86,39 @@ module.exports = function(grunt) {
     delete process.env.localOption;
   });
 
+  grunt.registerTask('testDirectives', function(){
+    assert.equal(process.env.ADD_NEGATIVE, 'should not change', 'add should not change existing env var');
+    assert.equal(process.env.ADD_POSITIVE, 'should be this', 'add should create new var if it does not exist');
+    assert.equal(process.env.PATHLIKE, 'foo:bar:baz:qux', 'extend should take delimiters into account');
+    assert.equal(process.env.UNSHIFTBASIC, 'foobar', 'should extend without specifying object');
+    assert.equal(process.env.UNSHIFTOBJECT, 'foobar', 'should extend while specifying object');
+    assert.equal(process.env.REPLACE, 'bar', 'replace should replace existing var');
+    delete process.env.ADD_NEGATIVE;
+    delete process.env.ADD_POSITIVE;
+    delete process.env.PATHLIKE;
+    delete process.env.UNSHIFTBASIC;
+    delete process.env.UNSHIFTOBJECT;
+    delete process.env.REPLACE;
+    delete process.env.globalOption;
+  });
+
   grunt.registerTask('writeDotEnv', function(){
-    grunt.file.write('.env', "dotEnvFileData=bar\ndotEnvFileOption=baz\n");
-    grunt.file.write('.env.json', '{"jsonValue" : "foo"}');
+    grunt.file.write('.env', "dotEnvFileData=bar\ndotEnvFileOption=baz");
+    grunt.file.write('.env.json', '{"jsonValue" : "foo","push" : {"PATHLIKE":"jsonPath"}}');
   });
 
   grunt.registerTask('testDotEnv', function(){
     assert(!process.env.src, 'Should not include src');
     assert.equal(process.env.jsonValue, 'foo', 'value from json env file should be set');
+    assert.equal(process.env.PATHLIKE, 'jsonPath', 'should process directives in json');
+    assert.equal(process.env.globalOption, 'foo', 'should still get global options');
     assert.equal(process.env.dotEnvFileData, 'bar', 'dotEnvFileData should be set');
     assert.equal(process.env.dotEnvFileOption, 'baz', 'dotEnvFileOption should be set');
     delete process.env.jsonValue;
     delete process.env.dotEnvFileData;
     delete process.env.dotEnvFileOption;
+    delete process.env.PATHLIKE;
+    delete process.env.globalOption;
   });
 
   // Default task.
@@ -77,6 +132,8 @@ module.exports = function(grunt) {
     'writeDotEnv',
     'env:testDotEnv',
     'testDotEnv',
+    'env:testDirectives',
+    'testDirectives',
     'clean'
   ]);
 
